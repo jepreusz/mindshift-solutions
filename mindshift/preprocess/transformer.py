88a7ddc8dfa51e209a4f6b0d948a3e529ce80f05
ankswarn <ankswarn@iu.edu>
2017-01-03 00:56:58 -0600

    Hierarchical clustering changes updated
    added cosine similarity function

--- mindshift/preprocess/transformer.py
+++ mindshift/preprocess/transformer.py
@@ -1,6 +1,7 @@
 # script to vectorize text and implement other transformation functions
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 class Transformer:
 
@@ -11,7 +12,13 @@ class Transformer:
     def vectorize_text(self, text):
         vectorized_text = self.vectorizer.fit_transform(text)
         self.vector_features = self.vectorizer.get_feature_names()
+        
         return vectorized_text
 
     def get_features(self):
         return self.vector_features
+        
+    def get_cosine(self,dtm):
+        dist = 1 - cosine_similarity(dtm)
+        return np.round(dist, 2)
+    
